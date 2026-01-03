5a901d62fc036fcbe14dfe9723ee2cde8877d7fe
yyykrk <y.ikemoto.krk@gmail.com>
2026-01-03 14:07:03 +0900

    feat: create SectionTitle component

---
+++ src/components/ui/SectionTitle.tsx
@@ -0,0 +1,22 @@
+import { Title } from "@mantine/core";
+
+type SectionTitleProps = {
+  children: React.ReactNode;
+};
+
+export const SectionTitle = ({ children }: SectionTitleProps) => {
+  return (
+    <Title
+      order={1}
+      style={{
+        textAlign: "center",
+        textDecoration: "underline",
+        textDecorationColor: "var(--mantine-color-green-8)",
+        textDecorationThickness: 3,
+        textUnderlineOffset: 9,
+      }}
+    >
+      {children}
+    </Title>
+  );
+};
