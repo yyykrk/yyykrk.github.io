d43bf05c88080a4798bd45dc5d7c6a2613549a20
yyykrk <y.ikemoto.krk@gmail.com>
2026-01-06 17:15:48 +0900

    bugfix: fix base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const repository = process.env.GITHUB_REPOSITORY?.split("/")?.[1];
-const base = repository ? `/${repository}/` : "/";
-
-// https://vite.dev/config/
 export default defineConfig({
-  base,
+  base: "/",
   plugins: [react()],
 });
