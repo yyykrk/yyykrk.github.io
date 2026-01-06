789851903da568898fed3c94dc0cb6dacb9cb192
yyykrk <y.ikemoto.krk@gmail.com>
2026-01-06 13:00:26 +0900

    bugfix: add base for github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const repository = process.env.GITHUB_REPOSITORY?.split("/")?.[1];
+const base = repository ? `/${repository}/` : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
-})
+});
